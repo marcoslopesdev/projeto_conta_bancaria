e185c3266ace8668c13a02cb7bad23c63ef5dbfc
Marcos <99marcoslopes@gmail.com>
2024-11-06 21:46:17 -0300

    task1 conta model

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -1,20 +1,29 @@
 package conta;
 
 import java.util.Scanner;
+import conta.model.Conta;
 import conta.util.Cores;
 
 public class Menu {
 
 	public static void main(String[] args) {
 
+		// Teste da Classe Conta
+		Conta c1 = new Conta(1, 123, 1, "Adriana", 10000.0f);
+		c1.visualizar();
+		c1.sacar(12000.0f);
+		c1.visualizar();
+		c1.depositar(5000.0f);
+		c1.visualizar();
+
 		Scanner leia = new Scanner(System.in);
-		
+
 		int opcao;
-			
-		while(true) {
-			
+
+		while (true) {
+
 			System.out.println(Cores.TEXT_YELLOW + Cores.ANSI_BLACK_BACKGROUND);
-			
+
 			System.out.println("-----------------------------------------------");
 			System.out.println("|                                             |");
 			System.out.println("|            BANCO DO BRAZIL COM Z            |");
@@ -31,69 +40,66 @@ public class Menu {
 			System.out.println("|      8 - Transferir valores entre contas    |");
 			System.out.println("|                                             |");
 			System.out.println("-----------------------------------------------" + Cores.TEXT_RESET);
-			
+
 			System.out.printf("Entre com a opção desejada: ");
 			opcao = leia.nextInt();
-			
-			if(opcao == 9) {
+
+			if (opcao == 9) {
 				System.out.println(Cores.TEXT_WHITE_BOLD + "\nBanco do Brazil com Z - O seu futuro começa aqui!");
 				sobre();
 				leia.close();
 				System.exit(0);
-				
-			switch(opcao) {
-			case 1:
-				System.out.println(Cores.TEXT_WHITE_BOLD + "Criar conta\n\n");
-				
-				break;
-			case 2:
-				System.out.println(Cores.TEXT_WHITE_BOLD + "Listar todas as contas\n\n");
-				
-				break;
-			case 3:
-				System.out.println(Cores.TEXT_WHITE_BOLD + "Consultar dados da conta - por número\n\n");
-				
-				break;
-			case 4:
-				System.out.println(Cores.TEXT_WHITE_BOLD + "Atualizar dados da conta\n\n");
-				
-				break;
-			case 5:
-				System.out.println(Cores.TEXT_WHITE_BOLD + "Apagar a conta\n\n");
-				
-				break;
-			case 6:
-				System.out.println(Cores.TEXT_WHITE_BOLD + "Saque\n\n");
-				
-				break;
-			case 7:
-				System.out.println(Cores.TEXT_WHITE_BOLD + "Depósito\n\n");
-				
-				break;
-			case 8:
-				System.out.println(Cores.TEXT_WHITE_BOLD + "Transferência entre contas\n\n");
-				
-				break;
-			default:
-				System.out.println(Cores.TEXT_RED_BOLD + "Opção inválida!\n");
-				break;
-			}
+
+				switch (opcao) {
+				case 1:
+					System.out.println(Cores.TEXT_WHITE_BOLD + "Criar conta\n\n");
+
+					break;
+				case 2:
+					System.out.println(Cores.TEXT_WHITE_BOLD + "Listar todas as contas\n\n");
+
+					break;
+				case 3:
+					System.out.println(Cores.TEXT_WHITE_BOLD + "Consultar dados da conta - por número\n\n");
+
+					break;
+				case 4:
+					System.out.println(Cores.TEXT_WHITE_BOLD + "Atualizar dados da conta\n\n");
+
+					break;
+				case 5:
+					System.out.println(Cores.TEXT_WHITE_BOLD + "Apagar a conta\n\n");
+
+					break;
+				case 6:
+					System.out.println(Cores.TEXT_WHITE_BOLD + "Saque\n\n");
+
+					break;
+				case 7:
+					System.out.println(Cores.TEXT_WHITE_BOLD + "Depósito\n\n");
+
+					break;
+				case 8:
+					System.out.println(Cores.TEXT_WHITE_BOLD + "Transferência entre contas\n\n");
+
+					break;
+				default:
+					System.out.println(Cores.TEXT_RED_BOLD + "Opção inválida!\n");
+					break;
+				}
 			}
 		}
-				
-			}
-			
-			public static void sobre() {
-				
-				System.out.println("------------------------------------------------");
-				System.out.println("|  Projeto desenvolvido por Marcos A.F. Lopes  |");
-				System.out.println("|  99marcoslopes@gmail.com                     |");
-				System.out.println("|  github.com/marcoslopesdev                   |");
-				System.out.println("------------------------------------------------");
-				
-			}	
-		
-		
+
 	}
 
+	public static void sobre() {
 
+		System.out.println("------------------------------------------------");
+		System.out.println("|  Projeto desenvolvido por Marcos A.F. Lopes  |");
+		System.out.println("|  99marcoslopes@gmail.com                     |");
+		System.out.println("|  github.com/marcoslopesdev                   |");
+		System.out.println("------------------------------------------------");
+
+	}
+
+}
